bf55d8df97132c1fe77a19cfd9d5ab359d82b0c6
Alibek Utyubayev <uas91@hotmail.com>
2015-03-03 03:32:14 -0500

    the first version of the main driver script that gets customer data(name, linkedin url, etc) from a text file, comletes that data with linkedin profile information, reads old data on the customers from a Mongo DB, sends old and new data to the classifier (not yet) to receive status, updates the customer data in DB, and triggers alert on status change. To-do: integrate with classifier, trigger alerts on specific changes only, allow user to add new customers (instead of asking the whole list) in a more convinient way and make the program work in the background and send alerts in a better way

--- handler.py
+++ handler.py
@@ -1,47 +1,64 @@
-# import AlibekApi
-# import YermekDB
+from linkedindev import *
+from read_data_from_capitalone import *
+from database import *
 # import BenClassifier
 import logging
-from read_data_from_capitalone import *
 
 logging.basicConfig(filename='handler.log', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 logging.info('Program has started')
 
-# data from capital one:    id
-#                           URL
-#                           last name
-#                           first name
-#                           dob
-
-# data from LinkedIn:       currentPosition []
-#                           industry
-#                           location
-#                           picture-url
-#                           maiden-name
-#                           headline
-
 
 logging.info('Setting up API')
 # setting up API
-# some code here...........
+inApi = linkedinApi()
 
 logging.info('Setting up DB')
 # setting up DB
-# some code here...........
+db = Database('mydb','customers','superAdm','123')
 
 logging.info('Reading file')
-dataList = read_capitalone_data('customer_info_capital_one.txt') # reading data from Capital One file into a list of dictionaries. (txt for now)
-db_data_list = []                                                # list of dictionaries for info from DB
 
-#for person in dataList:
-    #tempDict = some_dictionary_from_LinkedIn(person['URL']) #getting basic profile form LinkedIn as a dictionary
-    #db_temp = some_dictionary_from_DB(person['id'])         #getting information from DB as a dictionary
-    #person.update(tempDict)                                 #updating dataList with info we got from LinkedIn
-    #db_data_list.append(db_temp)                            #creating a list of dictionaries from info we got from DB
+# reading data from Capital One file into a list of dictionaries, will store new data
+data = read_capitalone_data('customer_info_capital_one.txt') 
+
+# initializing a list of dictionaries for info from DB, stores old data
+dbData = []                                                
+
+for person in data:
+    # getting linkedin profiles
+    profileData = inApi.getProfile(person['URL'])
+    
+    # adding data fields from linkedin profile to the initial customers data 
+    person.update(profileData) 
+    
+    # getting information from DB as a dictionary
+    db_temp = db.getCustomerById(person['id'])     
+    # adding to the list of data from DB 
+    dbData.append(db_temp)                        
 
 
-#for i in range(len(dataList)):
+for i in range(len(data)):
+    person = data[i]
+
+    # get status
     #status = call clasify with dataList[i] and db_data_list[i] and get some status
-
-    #for each person check if(status != datalist[i]['status']) ALERT!
-    #else push dataList[i] to DB
+    
+    status = 'test3'
+    
+    # assign status to the customer in the data list
+    person['status'] = status
+    
+    # update or insert customer in db (status and any changes from linkedin)
+    if dbData[i] == None:
+        db.insertCustomer(person)
+        oldStatus = None
+    else:
+        db.updateCustomer(person)
+        oldStatus = dbData[i]['status']
+     
+    # check if status has changed
+    # trigger alert on any change for now, later only on specific changes
+    if oldStatus != status:
+        print "Alert for " + person['firstName'] + ": changed to " + str(status) + " from " + str(oldStatus)
+    
+db.closeDB()
