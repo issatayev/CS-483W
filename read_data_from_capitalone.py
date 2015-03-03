c6e9fb48c08277ae7a8be68613cd2eacc57307a1
Alibek Utyubayev <uas91@hotmail.com>
2015-03-03 03:18:42 -0500

    changed read_capitalone_data function to accept fileName parameter

--- read_data_from_capitalone.py
+++ read_data_from_capitalone.py
@@ -5,18 +5,18 @@
 #   output: a list of dictionaries where each dictionary contains fields
 #			which represents a customer's basic info
 #
-def read_capitalone_data():
+def read_capitalone_data(fileName):
 	list_of_customers = []
-	datafile = open('customer_info_capital_one.txt', 'r')
-	list_of_data_from_datafile = datafile.readlines()
-	for i in range (len(list_of_data_from_datafile)):
-		single_line = list_of_data_from_datafile[i]
+        with open(fileName, 'r') as datafile:
+	    list_of_data_from_datafile = datafile.readlines()
+	    for i in range (len(list_of_data_from_datafile)):
+	        single_line = list_of_data_from_datafile[i]
 		single_line_split = single_line.split()
 		one_customer_info = {'id': single_line_split[0],
 						 'URL': single_line_split[1], 
-						 'last name': single_line_split[2], 
-						 'first name': single_line_split[3],
+						 'lastName': single_line_split[2], 
+						 'firstName': single_line_split[3],
 						 'dob': single_line_split[4] }
 		list_of_customers.append(one_customer_info)
-	return list_of_customers
+        return list_of_customers
 
