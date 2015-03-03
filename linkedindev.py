8a99bcc6f2a3a9b25e01c2f8d4c69384ac95e4b0
Alibek Utyubayev <uas91@hotmail.com>
2015-03-03 03:13:15 -0500

    changed to a class with properties and methods

--- linkedindev.py
+++ linkedindev.py
@@ -1,37 +1,20 @@
 from linkedin import linkedin
 
-# returns a dictionary object containing basic profile info for user whose public profile url is given
-# also requires linkedin application (dev or user) object  
-def get_basic_info(application, url):
-    return application.get_profile(member_url = url, selectors = ['id', 'first-name', 'last-name', 'location', 'distance', 'num-connections', 'industry', 'email-address', 'headline', 'positions'])
+class linkedinApi:
+    CONSUMER_KEY = '77il23ybk7gobx' 
+    CONSUMER_SECRET = 'fFnsP7fCRhwV8Nst'
+    USER_TOKEN = '35ab1da7-4bc7-4ff8-91bf-c21f0acfa010'
+    USER_SECRET = '8b461c18-eadb-4f49-9135-76053bb03446'
+    RETURN_URL = 'http://localhost:8000'
+    
+    # initialize an instance
+    def __init__(self):
+        self.authentication = linkedin.LinkedInDeveloperAuthentication(self.CONSUMER_KEY, self.CONSUMER_SECRET,
+                                                                        self.USER_TOKEN, self.USER_SECRET,
+                                                                        self.RETURN_URL, 
+                                                                        linkedin.PERMISSIONS.enums.values())
+        self.application = linkedin.LinkedInApplication(self.authentication)
 
-
-# Set up for application object
-# Probably need to create a separate class or a function to initiate parameters
-# and create application object (might just check if application was created with every request,but it's inefficient)
-
-CONSUMER_KEY = '77x34huvh2z8vr' 
-CONSUMER_SECRET = 'GhmHnUBy1E7fCBlp'
-USER_TOKEN = '899f8135-6c20-40bb-8e0d-601751d6ce2f'
-USER_SECRET = '76e88455-ba62-4a51-b59b-9dd6f04aeee4'
-RETURN_URL = 'http://localhost:8000'
-
-# Instantiate the developer authentication class object
-
-authentication = linkedin.LinkedInDeveloperAuthentication(CONSUMER_KEY, CONSUMER_SECRET, 
-                                                          USER_TOKEN, USER_SECRET, 
-                                                          RETURN_URL, linkedin.PERMISSIONS.enums.values())
-
-# Create an application object (static field of a global class would be better ...)
-
-application = linkedin.LinkedInApplication(authentication)
-
-
-url = 'https://www.linkedin.com/pub/yermek-issatayev/61/927/800'
-
-# Pass the app to the function
-data =  get_basic_info(application, url)
-
-name = data['firstName']
-
-print 'Here is some publicly available info about ' + name + ': ' + str(data)
+    # returns a dictionary with profile info for the user whose public profile url is given
+    def getProfile(self, url):
+        return self.application.get_profile(member_url = url, selectors = ['location', 'industry', 'headline', 'positions', 'maiden-name', 'picture-url'])
