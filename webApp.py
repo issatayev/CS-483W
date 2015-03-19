3d2d5ed5a2ab6a92deab292818685c6042e8f4e7
Alibek Utyubayev <uas91@hotmail.com>
2015-03-19 11:17:51 -0400

    changes to UI

--- webApp.py
+++ webApp.py
@@ -10,29 +10,32 @@ db = Database('mydb','customers','superAdm','123')
 
 # Define a route for the default URL, which loads the form
 @app.route('/')
-def form():
-    return render_template('form_submit.html')
+def list():
+    names = db.getAllCustomers()
+
+    return render_template('customers.html', names = names)
 
 # Define a route for the action of the form, for example '/hello/'
 # We are also defining which type of requests this route is 
 # accepting: POST requests in this case
 @app.route('/process/', methods=['POST'])
 def process():
-    name=request.form['name']
-    ID=request.form['id']
+    firstName = request.form['firstName']
+    lastName = request.form['lastName']
+    ID = request.form['id']
+    url = request.form['url']
+
+    if firstName == '' or lastName == '' or ID == '' or url == '':
+        return 'One of the fields in the form was missing'
+
+    person = {'id':ID, 'firstName':firstName,'lastName':lastName,'url':url,'status':''}
+    
     if db.getCustomerById(ID) == None:
-        db.insertCustomer({'id': ID, 'firstName':name, 'status':''})
+        db.insertCustomer(person)
     else:
-        db.updateCustomer({'id': ID, 'firstName':name, 'status':''})
+        db.updateCustomer(person)
 
-    return render_template('form_action.html', ID=ID, name = name)
-
-# list all data
-@app.route('/list/')
-def list():
-    names = db.getAllCustomers()
-
-    return render_template('customers.html', names = names)
+    return redirect(url_for('list')
 
 if __name__ == '__main__':
     app.run()
